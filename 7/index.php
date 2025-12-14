84390f35c2fa4efec3053fc6c78a100826080360
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2025-12-14 14:36:54 +0600

    added task 7

--- 7/index.php
+++ 7/index.php
@@ -9,9 +9,34 @@
     <?php 
     for($i=0; $i<3;$i++ ){
         for($j=0; $j<=$i; $j++ ){
-            echo "*"
-        }
+            echo "*";
+        } 
+        echo "<br>";
     }
+
+    
+    for ($i = 3; $i >= 1; $i--) {
+        for ($j = 1; $j <= $i; $j++) {
+            if ($i == 3 && $j < $i) {
+                echo $j . " ";
+            } else {
+                echo $j;
+            }
+        }
+        echo "<br>";
+    }
+    
+    $ch = 'A';
+    for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo $ch . " ";
+        $ch++;
+    }
+    echo "<br>";
+}
+
+
+    
 
      ?>
 </body>
